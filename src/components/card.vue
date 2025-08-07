a9e1a5753cf1be1a9116b77e05c1c6ba859b9f66
Setpars <153271014+Setpars@users.noreply.github.com>
2025-08-08 00:24:02 +0200

    Fix: image loading + reactive vendeur list + style update

--- src/components/card.vue
+++ src/components/card.vue
@@ -5,7 +5,7 @@ const vendeurs =ref([
   {
     name: "Mama Mado - Légumes frais",
     description: "Des légumes bio chaque jour au marché central de Likasi.",
-    image: "./assets/image/pexels-nc-farm-bureau-mark-2255935.jpg",
+    image: "@/assets/image/pexels-nc-farm-bureau-mark-2255935.jpg",
     typeActivité:"vente",
     adresse:'LIKASI',
     whatsapp: "+243831271968"
@@ -15,7 +15,7 @@ const vendeurs =ref([
     name: "Tony - Vêtements pour hommes",
     description: "Vêtements classe et pas chers. Livraison dans toute la ville.",
     typeActivité:"vente",
-    image: "./assets/image/pexels-hngstrm-1210484.jpg",
+    image: "@/assets/image/pexels-hngstrm-1210484.jpg",
     adresse:'LIKASI',
     whatsapp: "+243831271968"
   },
@@ -23,7 +23,7 @@ const vendeurs =ref([
     name: "Bakery Likasi",
     description: "Pain, gâteaux, croissants... Livraison possible.",
     typeActivité:"vente",
-    image: "./assets/image/pexels-busenur-demirkan-766536648-33136498.jpg",
+    image: "@/assets/image/pexels-busenur-demirkan-766536648-33136498.jpg",
     adresse:'LIKASI',
     whatsapp: "+243831271968"
 
